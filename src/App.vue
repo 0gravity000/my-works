6c1f1ae4297aa31b18e6ac318cc3929c7d8849a5
0gravity000 <0gravit000@gmail.com>
2022-04-14 14:31:41 +0900

    NavbarMainコンポーネントを追加

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,19 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#">Navbar</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <router-link to="/">
-              <a class="nav-link active" aria-current="page" href="#">Home</a>
-            </router-link>
-          </li>
-          <li class="nav-item">
-            <router-link to="/about">
-              <a class="nav-link" href="#">About</a>
-            </router-link>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
-
+  <NavbarMain />
   <router-view/>
 </template>
+
+<script>
+// @ is an alias to /src
+import NavbarMain from '@/components/NavbarMain.vue'
+
+export default {
+  components: {
+    NavbarMain
+  }
+}
+
+</script>
 
 <style>
 #app {
